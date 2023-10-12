af8a6297a81f8279f8a26e4801e406ee116d86ea
Anthony Martinez <1234567anthony1234567@gmail.com>
2023-10-11 23:04:07 -0700

    query scripts + processed map data as csv

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Path to the file containing the Overpass QL query
-QUERY_FILE="streets_and_intersections.overpassql"
+QUERY_FILE="query.overpassql"
 
 # Read the query from the file
 QUERY=$(cat "$QUERY_FILE")
 
-# Run the query and save the JSON output to a file
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" --data "data=${QUERY}" https://overpass-api.de/api/interpreter -o ../data/fullerton_map_data.json
-
-echo "Query completed. Results saved to fullerton_map_data.json"
+# Run the nodes query and save the JSON output to a file
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" --data "data=${QUERY}.intersections out;" https://overpass-api.de/api/interpreter -o mapdata.json
+echo "Query completed. Results saved to mapdata.json"
